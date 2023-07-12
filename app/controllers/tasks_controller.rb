4efc6850f625592ff7ae5924695a999402de5bb2
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-12 13:53:36 +0900

    削除機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,6 +30,14 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    # Rails7からレスポンスのリダイレクトにstatus: :see_otherをつける必要があるようです。
+    # https://qiita.com/jnchito/items/5c41a7031404c313da1f#destroy%E3%81%AE%E3%83%AC%E3%82%B9%E3%83%9D%E3%83%B3%E3%82%B9%E3%81%AB-status-see_other-%E3%82%92%E4%BB%98%E3%81%91%E3%82%8B%E5%BF%85%E8%A6%81%E3%81%8C%E3%81%82%E3%82%8B
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。", status: :see_other
+  end
+
   private
 
   def task_params
