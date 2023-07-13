6f05a0691c4e49818190ef5fec4883171b5c2268
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-13 11:12:03 +0900

    バリデーション失敗時にエラーを表示するよう変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    # flash[:notice] = "タスク「#{task.name}」を登録しました。"
-    # redirect_to tasks_url
-    # 上と一緒
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
