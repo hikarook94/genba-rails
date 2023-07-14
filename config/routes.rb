3e76d94ed246bf0085d3bf2474c94657dbe9b8f0
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-14 15:51:07 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   namespace :admin do
     resources :users
   end
 
   resources :tasks
-  root to: 'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
